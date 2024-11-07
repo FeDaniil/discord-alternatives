70465a0685ba7fbfa75d7d15021f684c22f18792
Fedorov Daniil <fedaniil@mail.ru>
2024-11-07 23:50:50 +0300

    Add mattermost playbook (untested)

--- setup.sh
+++ setup.sh
@@ -5,9 +5,18 @@ while IFS== read -r key value; do
 done <.env
 # install ansible
 echo "Installing Python and Ansible..."
-sudo apt install python3
+sudo apt update
+sudo apt install python3, unzip
 python3 -m pip install ansible
 # download and run ansible scripts
 if [[ $APP = "mattermost" ]]; then
   echo "Installing Mattermost..."
+else
+  echo "Unknown app. Currently only mattermost and rocketchat are supported."
+  exit 1
 fi
+curl -o ./ansible.zip "https://download-directory.github.io?url=https://github.com/FeDaniil/discord-alternatives/tree/master/$APP/ansible"
+unzip -q ansible.zip
+cd ansible
+ansible-playbook -i inventory install.yml
+echo "Done!"
